e3462ea4a4d7c7de083455cb28e383f04e177594
A R <A R@LAPTOP-M0TJ75U0>
2022-03-18 13:11:44 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sataic_Constructor
+{
+    class Program
+    {
+
+        static int id=1;
+        static string name="Ashish Ranjan";
+        static Program()
+        {
+            Console.WriteLine("Your Id is: "+id);
+            Console.WriteLine("Your Name is: " + name);
+        }
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            Console.ReadLine();
+        }
+    }
+}
